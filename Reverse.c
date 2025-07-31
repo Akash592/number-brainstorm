bf26995673a8b9f5ecab48fb93f42fa9245b9c86
Akash Magunde <134123542+Akash592@users.noreply.github.com>
2025-07-31 14:45:23 +0530

    Add files via upload

---
+++ Reverse.c
@@ -0,0 +1,19 @@
+
+// Author : Akash Dadaso Malgunde
+//Date    : 31/07/2025
+//Time    : 14:43
+
+
+#include <stdio.h>
+
+int main() {
+    
+    int iCnt = 0;
+    int n = 10;
+    for(iCnt = n; iCnt >= 1; iCnt--)
+    {
+        printf("%d\n",iCnt);
+    }
+
+    return 0;
+}
